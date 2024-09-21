d04e834f6cef14333c0fa30da8628ba7f5652414
bbagnes <bbagnes79@gmail.com>
2024-09-21 15:05:40 -0600

    Changed program according to activity 3 guidelines.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+                
+        string response = "yes";
+
+        do 
+        {
+            Random randomGenerator = new Random();
+            int magic = randomGenerator.Next(1, 101);
+
+            Console.Write("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            int guess = int.Parse(guessNumber);
+            int count = 1;
+
+            while (guess != magic)
+            {
+                if (guess > magic)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magic)
+                {
+                    Console.WriteLine("Higher");
+                }
+                
+                count ++;
+                Console.Write("What is your guess? ");
+                guessNumber = Console.ReadLine();
+                guess = int.Parse(guessNumber);
+                
+            }
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {count} guesses.");
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
+        Console.WriteLine("");
+        Console.WriteLine("Thank you for playing. Have a good day.");
     }
 }
