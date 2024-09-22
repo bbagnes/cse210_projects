b8a8e4aaf968defb0fc6a824eb12ab8fec0359df
bbagnes <bbagnes79@gmail.com>
2024-09-21 21:28:23 -0600

    Changed program according to directions provided for activity.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,45 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    {   
+        DisplayWelcome();
+
+        string UserName = PromptUserName();
+        int Number = PromptUserNumber();
+        int SquareRoot = SquareNumber(Number);
+
+        DisplayResult(UserName, SquareRoot); 
+               
+        static void DisplayWelcome ()
+            {
+                Console.WriteLine("Welcome to the Program!");
+            }
+
+        static string PromptUserName()
+            {
+                Console.Write("Please enter your name: ");
+                string UserName = Console.ReadLine();
+                return UserName;
+            }
+
+        static int PromptUserNumber()
+            {
+                Console.Write("Please enter your favorite number: ");
+                string UserNumber = Console.ReadLine();
+                int Number = int.Parse(UserNumber);
+                return Number;
+            }
+        static int SquareNumber(int Number)
+            {
+                int SquareRoot = Number * Number;
+                return SquareRoot;
+
+            }
+
+        static void DisplayResult(string UserName, int SquareRoot)
+            {
+                Console.WriteLine($"{UserName}, the square of your number is {SquareRoot}");
+            }      
+             
     }
 }
