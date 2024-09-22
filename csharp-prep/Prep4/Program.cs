9b1582f20a5a4c9efa93db01caf0112bb1a2fe5e
bbagnes <bbagnes79@gmail.com>
2024-09-21 19:00:03 -0600

    Changed program in accordance with core requirements of activity. Did not complete the stretch challenge.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<float> numbers = new List<float>() ;
+        string addNumber;
+        float number;
+        float sum = 0;
+        float max = -1;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            
+            Console.Write("Enter number: ");
+            addNumber = Console.ReadLine();
+            number = float.Parse(addNumber);
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }            
+            
+
+        } while (number != 0);
+
+        foreach (float value in numbers)
+        {
+            sum = sum + value;
+            if (value > max)
+            {
+                max = value;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}.");
+
+        float average = sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}.");
+        Console.WriteLine($"The largest number is: {max}.");
+    }    
 }
