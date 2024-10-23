9be193f53d8ec9476833fc85f768bf901e1ceacd
bbagnes <bbagnes79@gmail.com>
2024-10-22 19:36:12 -0600

    Implimented and tested a display animation.

---
+++ sandbox/Sandbox/Activity.cs
@@ -0,0 +1,118 @@
+using System;
+
+
+public class Activity
+{
+    private string _name = "";
+    private string _description = "";
+    protected int _duration = 0;
+    
+
+    public Activity(string name, string description, int duration)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+
+    public void DisplayStartMessage()
+    {
+            Console.WriteLine("");
+            Console.WriteLine($"Welcome to the {_name}.");
+            Console.WriteLine("");
+            Thread.Sleep(1000);
+            Console.WriteLine(_description);
+            Thread.Sleep(2000);
+            Console.WriteLine("");
+
+            Console.Write("How long, in seconds, would you like for your session to last? ");
+            string programDuration = Console.ReadLine();
+            _duration = int.Parse(programDuration);
+    }
+
+    public void DisplayEndMessage()
+    {
+        Console.WriteLine("Well Done!");
+
+        ShowSpinner(2);
+
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_name}.");
+
+        ShowSpinner(2);
+    }
+
+    public void ShowSpinner(int countdown)
+    {
+    
+        int countDown = countdown;
+        while (countDown > 0)
+        {  
+            Console.Write("\b \b");
+            Console.Write("/");
+            Thread.Sleep(500);
+
+            Console.Write("\b \b");
+            Console.Write("——");
+            Thread.Sleep(500);
+
+            Console.Write("\b\b  \b\b");
+            Console.Write(@"\");
+            Thread.Sleep(500);
+            
+            Console.Write("\b \b");
+            Console.Write("|");
+            Thread.Sleep(500);
+
+            countDown --;
+        }
+        Console.Write("\b \b");
+        Console.WriteLine("");
+    }
+
+    public void ShowCountDown(int count)
+    {
+        int countDown = count;
+        while (countDown > 0)
+        {  
+            Console.Write("\b \b");
+            Console.Write($"{countDown}");
+            Thread.Sleep(1000);
+
+            countDown --;
+        }
+        Console.Write("\b \b");
+        Console.WriteLine("");
+    
+    }
+
+    public void ShowTwinklingStars()
+        {    
+            int countDown = 30;
+            while (countDown > 0)
+            {
+                Console.Write("\b\b\b\b\b     \b\b\b\b\b");
+                Console.Write("' ' '");
+                Thread.Sleep(1000);
+    
+                Console.Write("\b\b\b\b\b     \b\b\b\b\b");
+                Console.Write("* ' '");
+                Thread.Sleep(1000);
+
+                Console.Write("\b\b\b\b\b     \b\b\b\b\b");
+                Console.Write("' * '");
+                Thread.Sleep(1000);
+
+                Console.Write("\b\b\b\b\b     \b\b\b\b\b");
+                Console.Write("' ' *");
+                Thread.Sleep(1000);
+
+                Console.Write("\b\b\b\b\b     \b\b\b\b\b");
+                Console.Write("* * *");
+                Thread.Sleep(1000);
+
+                countDown --;
+            }
+            Console.Write("\b \b");
+            Console.WriteLine("");
+        }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
+     
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Activity activity = new Activity("activity", "This activity is purely for the purposes of demonstrating the Twinkling Stars method", 30);
+        
+        activity.DisplayStartMessage();
+        Console.WriteLine("");        
+        activity.ShowTwinklingStars();
+        Console.WriteLine("");
     }
 }
