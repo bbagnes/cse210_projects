a95da235507dc837a37b4be549e87d073ab3c44f
bbagnes <bbagnes79@gmail.com>
2024-10-22 19:37:46 -0600

    Completed week 6 learning activity according to directions, but not in compliance with supplied example; which switched Shapes class to Abstract.

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Reflection.Metadata;
 
 class Program
 {
@@ -8,5 +10,23 @@ class Program
         Square sq = new Square("red", 5);
         Console.WriteLine(sq.GetColor());
         Console.WriteLine(sq.GetArea());
+
+        Circle cc = new Circle("blue", 5);
+        Console.WriteLine(cc.GetColor());
+        Console.WriteLine(cc.GetArea());
+
+        Rectangle rc = new Rectangle("green", 5, 7);
+        Console.WriteLine(rc.GetColor());
+        Console.WriteLine(rc.GetArea());
+
+        List<Shape> shapes = new List<Shape>() {sq, cc, rc};
+        
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine("");
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine("");
+        }
     }
 }
